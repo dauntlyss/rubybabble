02a90db9b76a7a4c37cb01533cb59762477b9158
Alyssa Harris <ah00214@my.westga.edu>
2023-01-19 20:14:30 -0500

    Added empty? and self.points methods

--- tile_bag.rb
+++ tile_bag.rb
@@ -40,4 +40,24 @@ class TileBag
     return @random_tile
   end
 
+  # Returns true if the bag is empty, false otherwise
+  def empty?
+    @is_bag_empty = @tile_bag.empty?
+    return @is_bag_empty
+  end
+
+  # Returns the points for a given tiles
+  def self.points_for(tile)
+    @tile_points = {
+      :A  1,  :B => 3,  :C => 3,
+      :D => 2,  :E => 1,  :F => 4,
+      :G => 2,  :H => 4,  :I => 1,
+      :J => 8,  :K => 5,  :L => 1,
+      :M => 3,  :N => 1,  :O => 3,
+      :P => 3,  :Q => 10,  :R => 1,
+      :S => 1,  :T => 1,  :U => 1,
+      :V => 4,  :W => 4,  :X => 8,
+      :Y => 4,  :Z => 10
+    }
+
 end

76d3eadeb848823a3e192cfee982a02d62e21ac6
Alyssa Harris <ah00214@my.westga.edu>
2023-01-20 16:54:21 -0500

    Created and passed test for tileGroup append method

---
+++ tests/tile_group/test_append.rb
@@ -0,0 +1,37 @@
+# Tests the append method of the TileGroup class
+
+require "minitest/autorun"
+require_relative "../../tile_group.rb"
+
+class TileGroup::TestAppend < Minitest::Test
+  # Used tp setup a new TileGroup before each test
+  def setup
+    @group = TileGroup.new
+  end
+
+  #Test that appends one tile to the group
+  def test_append_one_tile
+    @group.append('A')
+    assert_equal "A", @group.hand
+  end
+
+  #Test that appends many tiles to the group
+  def test_append_many_tiles
+    @group.append('R')
+    @group.append('S')
+    @group.append('T')
+    @group.append('L')
+    @group.append('N')
+    @group.append('E')
+    @group.append('E')
+    assert_equal "RSTLNEE", @group.hand
+  end
+
+  #Test that appends duplicate tiles to the group
+  def test_append_duplicate_tiles
+    @group.append('E')
+    @group.append('E')
+    @group.append('E')
+  end
+  
+end

c115a9bfc25c02da4a97c44fdc4b628317196911
Alyssa Harris <ah00214@my.westga.edu>
2023-01-20 13:14:12 -0500

    Corrected failing test and refactored test method with arrays

--- tests/tile_bag/test_points_for.rb
+++ tests/tile_bag/test_points_for.rb
@@ -11,32 +11,31 @@ class TileBag::TestPointsFor < Minitest::Test
 
   # Test to check the point values returned for each letter
   def test_confirm_point_values
-    assert_equal 1, TileBag.points_for('A')
-    assert_equal 3, TileBag.points_for('B')
-    assert_equal 3, TileBag.points_for('C')
-    assert_equal 2, TileBag.points_for('D')
-    assert_equal 1, TileBag.points_for('E')
-    assert_equal 4, TileBag.points_for('F')
-    assert_equal 2, TileBag.points_for('G')
-    assert_equal 4, TileBag.points_for('H')
-    assert_equal 1, TileBag.points_for('I')
-    assert_equal 8, TileBag.points_for('J')
-    assert_equal 5, TileBag.points_for('K')
-    assert_equal 1, TileBag.points_for('L')
-    assert_equal 3, TileBag.points_for('M')
-    assert_equal 1, TileBag.points_for('N')
-    assert_equal 3, TileBag.points_for('O')
-    assert_equal 3, TileBag.points_for('P')
-    assert_equal 10, TileBag.points_for('Q')
-    assert_equal 1, TileBag.points_for('R')
-    assert_equal 1, TileBag.points_for('S')
-    assert_equal 1, TileBag.points_for('T')
-    assert_equal 1, TileBag.points_for('U')
-    assert_equal 4, TileBag.points_for('V')
-    assert_equal 4, TileBag.points_for('W')
-    assert_equal 8, TileBag.points_for('X')
-    assert_equal 4, TileBag.points_for('Y')
-    assert_equal 10, TileBag.points_for('Z')
+    [:A, :E, :I, :L, :N, :R, :S, :T, :U].each do |tile|
+      assert_equal 1, TileBag.points_for(tile)
+    end
+
+    [:D, :G].each do |tile|
+      assert_equal 2, TileBag.points_for(tile)
+    end
+
+    [:B, :C, :M, :O, :P].each do |tile|
+      assert_equal 3, TileBag.points_for(tile)
+    end
+
+    [:F, :H, :V, :W, :Y].each do |tile|
+      assert_equal 4, TileBag.points_for(tile)
+    end
+
+    assert_equal 5, TileBag.points_for(:K)
+
+    [:J, :X].each do |tile|
+      assert_equal 8, TileBag.points_for(tile)
+    end
+
+    [:Q, :Z].each do |tile|
+      assert_equal 10, TileBag.points_for(tile)
+    end
   end
-  
+
 end

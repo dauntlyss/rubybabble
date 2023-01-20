4540364ab46d6c4b977f611aae3d875fbfebf40c
Alyssa Harris <ah00214@my.westga.edu>
2023-01-20 12:39:13 -0500

    Added unit tests for points_for method

--- tile_bag.rb
+++ tile_bag.rb
@@ -46,10 +46,10 @@ class TileBag
     return @is_bag_empty
   end
 
-  # Returns the points for a given tiles
+  # Returns the point values for a given tiles
   def self.points_for(tile)
     @tile_points = {
-      :A  1,  :B => 3,  :C => 3,
+      :A => 1,  :B => 3,  :C => 3,
       :D => 2,  :E => 1,  :F => 4,
       :G => 2,  :H => 4,  :I => 1,
       :J => 8,  :K => 5,  :L => 1,
